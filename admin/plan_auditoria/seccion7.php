6be340c32b8a2f4dfad191da2963af55f4fe6af3
Malpika2 <hamthony@hotmail.com>
2017-05-04 16:56:25 -0500

    Comió

--- admin/plan_auditoria/seccion7.php
+++ admin/plan_auditoria/seccion7.php
@@ -1,6 +1,6 @@
 <fieldset>
-<div class="row" id="seccion7" style="background-color: #ecfbe7;">
-  <div class="col-lg-12 col-xs-12" id="tabla_ajax2">
+<div class="row" id="tabla_ajax2" style="background-color: #ecfbe7;">
+  <div class="col-lg-12 col-xs-12">
     <?
       include('tabla2.php');
     ?>
